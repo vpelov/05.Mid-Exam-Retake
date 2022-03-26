f6fe416383bb47aac3cf704a988c132c19fb7040
VladimirPelov <vpelov@mail.bg>
2022-03-26 16:32:09 +0200

    Add files via upload

---
+++ Exam_04.2022_Exercises/P02.DestinationMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\=|\/)([A-Z]{1}[A-Za-z]{2,})(\1)";
+            List<string> travelPoint = new List<string>();
+            int countAllLenght = 0;
+
+            string travelData = Console.ReadLine();
+            MatchCollection match = Regex.Matches(travelData, pattern);
+
+            foreach (Match item in match)
+            {
+                string current = item.Value;
+                current = current.Remove(0, 1);
+                current = current.Remove(current.Length - 1, 1);
+                countAllLenght += current.Length;
+
+                travelPoint.Add(current);
+            }
+
+            Console.Write("Destinations: ");
+
+            Console.Write(String.Join(", ", travelPoint));
+            Console.WriteLine();
+            Console.WriteLine($"Travel Points: {countAllLenght}");
+
+        }
+    }
+}
