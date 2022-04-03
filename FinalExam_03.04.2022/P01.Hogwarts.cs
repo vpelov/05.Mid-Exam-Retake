62dfd5d6e52fa82bd3422821e3687aeed158c73c
VladimirPelov <vpelov@mail.bg>
2022-04-03 18:11:10 +0300

    Add files via upload

---
+++ FinalExam_03.04.2022/P01.Hogwarts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.Hogwarts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greet = "Hello World";
+            Console.WriteLine(greet.Substring(3, 4));
+
+        }
+    }
+}
